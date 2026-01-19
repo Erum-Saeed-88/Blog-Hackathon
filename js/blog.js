f622dc317085dc48ef81723f32966eac3af16bdd
ErumSaeed88 <tawassuljunaid1988@gmail.com>
2026-01-19 07:52:09 +0500

    Add files via upload

--- js/blog.js
+++ js/blog.js
@@ -6,55 +6,78 @@ export const babyProducts = [
     description: "Complete your baby care kit with diapers & bottle...",
     imageUrl: "https://thumbs.dreamstime.com/b/baby-essentials-including-diapers-rattle-bottle-pacifier-collection-essential-items-stack-colorful-pastel-background-415583413.jpg",
     fullDetail: "This Hypoallergenic diapers + BPA-free bottle set is perfect choice for 0-6 months babies.",
-    price:"$290.00"
+    price: 499,
+    stock: 15,
+    rating: 5.9
   },
   {
     id: 2,
     title: "Baby Shoes",
     description: "Beautiful Foam colour shoes...",
     imageUrl: "https://res.cloudinary.com/dxmaagsgg/image/upload/v1768623609/baby-shoes_kiphb9.webp",
-    fullDetail: "Trendy Softstyle baby shoes for comfortable steps..!"
+    fullDetail: "Trendy Softstyle baby shoes for comfortable steps..!",
+    price:503,
+    stock: 18,
+    rating: 4.3
   },
   {
     id: 3,
     title: "Newborn Baby Basket",
     description: "Best Choice for baby collections...",
     imageUrl: "https://res.cloudinary.com/dxmaagsgg/image/upload/v1768623499/baby-gift_iq1hhc.webp",
-    fullDetail: "Carry your baby's accessories with safety & comfort."
+    fullDetail: "Carry your baby's accessories with safety & comfort.",
+    price:595,
+    stock:15,
+    rating: 3.5
   },
   {
     id: 4,
     title: "Baby Wooden Toys",
     description: "Best Quality Wooden Toy set...",
     imageUrl: "https://res.cloudinary.com/dxmaagsgg/image/upload/v1768623627/baby-toys_jvk75q.webp",
-    fullDetail: "Best choice for your baby, play with safe & beautiful materials."
+    fullDetail: "Best choice for your baby, play with safe & beautiful materials.",
+    price:495,
+    stock:18,
+    rating: 8.5
   },
   {
     id: 5,
     title: "Baby Stroller",
     description: "Flat lay with pacifiers essentials...",
     imageUrl: "https://res.cloudinary.com/dxmaagsgg/image/upload/v1768623581/Baby-Stroller_plpwdk.webp",
-    fullDetail: "Imported Baby items, feel your baby too much relexing ."
+    fullDetail: "Imported Baby items, feel your baby too much relexing .",
+    price:995,
+    stock:20,
+    rating: 7.9
   },
   {
     id: 6,
     title: "Cute & Soft Toys",
     description: "Soft colours with soft touch...",
     imageUrl: "https://res.cloudinary.com/dxmaagsgg/image/upload/v1768623678/baby-softToy_r7vj5c.avif",
-    fullDetail: "Bright colours with soft touch, your baby play & sleep with it."
+    fullDetail: "Bright colours with soft touch, your baby play & sleep with it.",
+    price:295,
+    stock:15,
+    rating: 6.5
   },
   {
     id: 7,
     title: "NeXton Bath Collection",
     description: "For Soft & Silky Feel to your baby...",
     imageUrl: "https://res.cloudinary.com/dxmaagsgg/image/upload/v1768724768/bath_set_jsidip.webp",
-    fullDetail: "NEXTON Baby Bath set gives a soft touch & Fresh fellings to your baby"
+    fullDetail: "NEXTON Baby Bath set gives a soft touch & Fresh fellings to your baby",
+    price:495,
+    stock:15,
+    rating: 7.5
   },
   {
     id: 8,
     title: "Cute Baby Comforter Set",
     description: "Beautiful grey colour Baby comforter set...",
     imageUrl: "https://res.cloudinary.com/dxmaagsgg/image/upload/v1768724876/boby-comforter_qveuzt.webp",
-    fullDetail: "Bright & Attractable colour with soft & comfortable touch."
+    fullDetail: "Bright & Attractable colour with soft & comfortable touch.",
+    price:600,
+    stock:15,
+    rating: 3.5
   }
 ];
